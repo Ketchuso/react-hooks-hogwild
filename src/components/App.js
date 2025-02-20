4e6069bf947b7b1afbcf963e0c1125fa4b3d68d3
Jay Davis <jdlikesbsbl@gmail.com>
2025-02-20 14:51:29 -0600

    Adds pig info when clicked on title

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,15 @@ import Title from "./Title"
 
 function App() {
 	const hog = hogs.map((hog) => {
-		return <Title name={hog.name} image={hog.image} key={hog.name}/>
+		return <
+			Title 
+			name={hog.name} 
+			specialty={hog.specialty}
+			greased={hog.greased}
+			weight={hog.weight}
+			highest_medal={hog["highest medal achieved"]}
+			image={hog.image} 
+			key={hog.name}/>
 	})
 
 	return (
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import { React, useState } from 'react';
 
-function Title({ name, image }){
+function Title({ name, image, specialty, greased, weight, highest_medal }){
+    const [isClick, setClick] = useState(false)
+    function handeClick(){
+        return setClick(true)
+    }
+
     return (
         <div>
-            <h1>{name}</h1>
+            <h1 onClick={handeClick}>{name}</h1>
+            {isClick ? <div><p>Speciatly: {specialty}</p><p>Greased: {greased ? "yes" : "no"}</p><p>Weight: {weight}</p><p>Highest medal: {highest_medal}</p></div> : null}
             <img src={image} alt={name}></img>
         </div>
     )
